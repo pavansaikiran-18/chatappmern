355b655a2e994274f6ef6885dba0f146a41fd8cd
pavansaikiran-18 <nerellapavansaikiran@gmail.com>
2025-03-25 16:49:12 +0530

    routing in react

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,28 @@
-import './global.css'
-import React from 'react'
+import "./global.css";
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Navbars from "./components/Navbars"; // Fixed import
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Loginpage";
+import Profile from "./pages/Profilepage";
+import Settings from "./pages/Settingpage";
+import Signup from "./pages/Signuppage";
 
 const App = () => {
   return (
-    <div>
-   <button className="btn btn-outline">Default</button>
-<button className="btn btn-outline btn-primary">Primary</button>
-<button className="btn btn-outline btn-secondary">Secondary</button>
-<button className="btn btn-outline btn-accent">Accent</button>
-<button className="btn btn-outline btn-info">Info</button>
-<button className="btn btn-outline btn-success">Success</button>
-<button className="btn btn-outline btn-warning">Warning</button>
-<button className="btn btn-outline btn-error">Error</button>
+    <>
+      <button className="btn">Default</button>
+      <Navbars />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/profile" element={<Profile />} /> {/* Lowercased for consistency */}
+      </Routes>
+      </>
     
-    </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
