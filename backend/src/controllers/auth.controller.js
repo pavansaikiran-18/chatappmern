7e7fbe6dfaca9193b0934c2c0eb78274f4e0f16b
pavansaikiran-18 <nerellapavansaikiran@gmail.com>
2025-05-12 23:38:40 +0530

    profile commit

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -90,6 +90,7 @@ export const updateProfile = async (req, res) => {
   try {
     const { profilePic } = req.body;
     const userId = req.user._id;
+    console.log(userId,profilePic)
 
     if (!profilePic) {
       return res.status(400).json({ message: "Profile pic is required" });
